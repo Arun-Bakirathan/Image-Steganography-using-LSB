52f182aaae0872dbe9cf4bb626e7844670e86e14
Arun <68496694+Arun-Bakirathan@users.noreply.github.com>
2021-01-26 13:42:11 +0530

    Add files via upload

---
+++ Using Java/Home.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class Home extends JFrame implements ActionListener
+{
+	private JButton compose,breakmsg;
+
+	Home()
+	{
+		
+		super("Steganography");
+		Image img =Toolkit.getDefaultToolkit().createImage("background.jpg");
+		Container con=getContentPane();
+		con.setLayout(null);
+		compose=new JButton("Encrypt");
+		compose.addActionListener(this);
+		compose.setBounds(300,350,150,50);
+		breakmsg=new JButton("Decrypt");
+		breakmsg.addActionListener(this);
+		breakmsg.setBounds(550,350,150,50);
+		con.add(compose);
+		con.add(breakmsg);
+	}
+
+	public void actionPerformed(ActionEvent ae)
+	{
+		if(ae.getSource()==compose)
+		{
+			this.dispose();
+			ComposePage cp=new ComposePage();
+			cp.setSize(1035,790);
+			cp.setVisible(true);
+		}
+
+		if(ae.getSource()==breakmsg)
+		{
+			this.dispose();
+			BreakPage bp=new BreakPage();
+			bp.setSize(1035,790);
+			bp.setVisible(true);
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		Home h=new Home();
+		h.setSize(1035,790);
+		h.setVisible(true);
+	}
+}
